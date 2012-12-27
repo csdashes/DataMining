5613a123b97a6f0bec4f92d21696ad2de9daab8f
nop33 <itrichop@csd.auth.gr>
2012-12-27 18:06:04 +0200

    First commit by me.

--- src/datamining/DataMining.java
+++ src/datamining/DataMining.java
@@ -15,5 +15,6 @@ public class DataMining {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Let's start coding");
     }
 }
