7449c517a06a1ff03daae003bfbdd8dc69a5ac5b
nop33 <itrichop@csd.auth.gr>
2012-12-27 17:33:39 +0200

    project created

---
+++ src/data_mining/DataMining.java
@@ -0,0 +1,13 @@
+package data_mining;
+
+public class DataMining {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello, world!");
+	}
+
+}
